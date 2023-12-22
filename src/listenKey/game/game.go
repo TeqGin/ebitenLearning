a22ab5543ad34f1be093dd534765a56bbc27b4c4
Ross <1102647596@qq.com>
2023-12-22 10:41:37 +0800

    add background

--- src/listenKey/game/game.go
+++ src/listenKey/game/game.go
@@ -5,44 +5,36 @@ import (
 )
 
 type Game struct {
-	i   *input
 	cfg *config
 	p   *plane
+	bg  *background
 }
 
-type input struct {
-	msg string
-}
+const (
+	resourcePath = "./resource"
+)
 
 func NewGame() *Game {
 	cfg := loadConfig()
 	ebiten.SetWindowSize(cfg.Width, cfg.Hight)
 	ebiten.SetWindowTitle(cfg.Title)
 	return &Game{
-		i:   &input{},
 		cfg: cfg,
-		p:   loadPlane("./resource/airplane/plane/plane1.png", cfg),
-	}
-}
-
-func (i *input) update() {
-	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		i.msg = "go right"
-	} else if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		i.msg = "go left"
-	} else if ebiten.IsKeyPressed(ebiten.KeySpace) {
-		i.msg = "go space"
+		p:   loadPlane(resourcePath+"/airplane/plane/plane1.png", cfg),
+		bg:  loadBackground(resourcePath + "/background/bg1.jpg"),
 	}
 }
 
 // update the running data
 func (g *Game) Update() error {
+	g.bg.update()
 	g.p.update(g.cfg)
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(g.cfg.BgColor)
+	g.bg.draw(screen, g.cfg)
 	g.p.Draw(screen, g.cfg)
 	// ebitenutil.DebugPrint(screen, g.i.msg)
 }
